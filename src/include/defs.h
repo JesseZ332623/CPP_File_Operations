3d12e41ba6f45e49c48f338dcc0bffb80b0b3d25
Jesse <3191955858@qq.com>
2024-01-20 23:24:12 +0800

    一些头文件，宏定义声明在此处

---
+++ src/include/defs.h
@@ -0,0 +1,12 @@
+#ifndef __DEFS_H_
+#define __DEFS_H_
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <algorithm>
+#include <chrono>
+
+#endif
