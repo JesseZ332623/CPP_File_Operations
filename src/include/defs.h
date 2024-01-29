c251e9cc655d86625e079c674546a5e6e1b53106
Jesse <3191955858@qq.com>
2024-01-29 15:11:36 +0800

    一些 macro，只读数据和一些辅助函数所在

--- src/include/defs.h
+++ src/include/defs.h
@@ -3,7 +3,6 @@
 
 #include <fstream>
 #include <vector>
-#include <string>
 #include <cctype>
 #include <algorithm>
 #include <chrono>
@@ -51,12 +50,15 @@
 
 /*
     菜单枚举类型，功能如下：
+
+    BACK    退回到输入文件路径的界面
     INSERT  插入数据到文件末尾
     LOAD    从文件加载数据到结构体，然后再排序结构体，最后输出至屏幕，文件或其他程序
+    DELETE  输入 Combination of Positives 在文件检索对应的行并删除该行
     SEARCH  根据 Combination of Positives 查询对应的数据
     QUIT    退出系统
 */
-enum MENU {INSERT = '1', LOAD, DELETE, SEARCH, QUIT = 'q'}; 
+enum MENU {BACK = '0', INSERT, LOAD, DELETE, SEARCH, QUIT = 'q'}; 
 
 /*
     表中的数据在结构体中使用类型如下：
@@ -77,7 +79,7 @@ struct PositiveConfidenceLimits
         int lower;
         int upper;
 
-        PositiveConfidenceLimits() {}
+        PositiveConfidenceLimits() = default;
 
         PositiveConfidenceLimits(std::string & __COP, int __mpnIndex, int __low, int __up) 
         : COP(__COP), mpnIndex(__mpnIndex), lower(__low), upper(__up) {}
@@ -98,6 +100,7 @@ const std::vector<const char *> tableKey =
 
 const std::vector<const char *> menuTable = 
 {
+    "0. Back to File PATH Input",
     "1. Insert data",
     "2. Load and display data",
     "3. Delete data",
@@ -119,5 +122,24 @@ typedef struct LogLevel
 
 /*一个简单的日志库，用于不同类型消息的输出*/
 static MyLog myLog;
+
+/**
+    * @brief                    自制的延时函数，来代替 Windows 库中的 Sleep 函数。
+    * 
+    * @param __millisSeconds    要延迟的时间，精确到毫秒
+    * 
+    * @return                   non-return
+*/
+void delayMilliseconds(unsigned long int __millisSeconds);
+
+ /**
+    * @brief            这个函数可以打印不同风格的分割线
+    * 
+    * @param __len      分割线的长度
+    * @param __style    分割线的风格，如（------- ****** 或 ·········）
+    * 
+    * @return           non-return
+*/
+void printSplitLine(int __len, const char __style);
 
 #endif // __DEFS_H_
