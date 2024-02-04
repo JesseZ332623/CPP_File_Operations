9f28992a76a4f7332e390fe55fa68e0468b7dee4
Jesse <3191955858@qq.com>
2024-02-04 19:17:28 +0800

    选项和菜单的操作所在

--- src/include/optionsAndMenu.h
+++ src/include/optionsAndMenu.h
@@ -1,6 +1,43 @@
 #ifndef _OPTIONS_AND_MENU_H_
 #define _OPTIONS_AND_MENU_H_
 
+#include <array>
+
+/*
+    菜单枚举类型，功能如下：
+
+    BACK    退回到输入文件路径的界面
+    INSERT  插入数据到文件末尾
+    LOAD    从文件加载数据到结构体，然后再排序结构体，最后输出至屏幕，文件或其他程序
+    DELETE  输入 Combination of Positives 在文件检索对应的行并删除该行
+    SEARCH  根据 Combination of Positives 查询对应的数据
+    QUIT    退出系统
+*/
+enum MENU {BACK = '0', INSERT, LOAD, DELETE, SEARCH, QUIT = 'q'}; 
+
+/*
+    对应表中的 4 个键，
+    有了这个字符串数组，就可以用循环来控制输入。
+    当然，这里不能用 printf 去读取，% C 有可能会被误认为格式字符。
+*/
+const std::array<const char *, 4> tableKey = 
+{
+    "Combination of Positives (Format: #-#-#)",
+    "MPN index (per 100 ml)",
+    "95% Confidence Limits of Lower",
+    "95% Confidence Limits of Upper"
+};
+
+const std::array<const char *, 6> menuTable = 
+{
+    "0. Back to File PATH Input",
+    "1. Insert data",
+    "2. Load and display data",
+    "3. Delete data",
+    "4. Search data",
+    "q. Exit system"
+};
+
 /**
  * @brief 在终端输出菜单
  * 
